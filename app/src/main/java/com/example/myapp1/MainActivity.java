f616598488ce59d8de99e73b66c60cc95bdfabe0
Supun Dileepa <71589526+supundileepa00@users.noreply.github.com>
2021-09-25 00:38:07 +0530

    display method

--- app/src/main/java/com/example/myapp1/MainActivity.java
+++ app/src/main/java/com/example/myapp1/MainActivity.java
@@ -18,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
+    public void displayDetails(){
+    
+    }
 
 
     //send message
